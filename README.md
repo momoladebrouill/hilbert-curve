d79638203d79ceee2a41a958a1a4496e90a70617
momoladebrouill <momoladebrouill@gmail.com>
2021-07-02 18:33:18 +0200

    J'ai fini
    C'était fun, prochaine étape , avec les positions !

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hilbert curve
  
+Dessiner un U
+le copier en 4 fois
+faire tourner haut gauche anticlock
+faire trouner droite clock
+relier gauche et droite, et bas
---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame as pg
+from pygame.locals import *
+pg.init()
+f=pg.display.set_mode((500,500))
+B=1
+fps=pg.time.Clock()
+f.fill(0)
+size=50
+margin=150
+pg.draw.line(f,0xffffff,(margin,margin),(margin,500-margin),size)
+pg.draw.line(f,0xffffff,(500-margin,margin),(500-margin,500-margin),size)
+pg.draw.line(f,0xffffff,(margin,500-margin),(500-margin,500-margin),size)
+
+pg.display.flip()
+fps.tick(1)
+s=parts=0
+pos=[(0,0),(250,0),(0,250),(250,250)]
+j=1
+margin/=2
+ 
+
+while B:
+    for event in pg.event.get():
+        if event.type==QUIT:
+            pg.quit()
+            B=0
+        elif event.type==pg.KEYUP:
+            f=pg.display.get_surface()
+            s=f.copy()
+            s=pg.transform.scale(s,(250,250))
+            parts=list([s.copy() for _ in range(4)])
+            f.fill(0)
+            for i in range(4):
+                if pos[i]==(0,0):
+                    parts[i]=pg.transform.rotate(parts[i],90)
+                elif pos[i]==(250,0):
+                    parts[i]=pg.transform.rotate(parts[i],-90)
+                f.blit(parts[i],pos[i])
+            pg.draw.line(f,0xffffff,(margin/j,250-margin/j),(margin/j,250+margin/j),round(size/(j*2)))
+            pg.draw.line(f,0xffffff,(500-margin/j,250-margin/j),(500-margin/j,250+margin/j),round(size/(j*2)))
+            pg.draw.line(f,0xffffff,(250-margin/j,250+margin/j),(250+margin/j,250+margin/j),round(size/(j*2)))
+            pg.display.flip()
+            j*=2
